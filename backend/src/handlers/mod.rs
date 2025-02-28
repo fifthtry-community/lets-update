114f160793f9d71f5af2e43a3fe55e32dc55b44f
Amit Upadhyay <upadhyay@gmail.com>
2025-02-28 13:56:17 +0530

    refactor

--- backend/src/handlers/mod.rs
+++ backend/src/handlers/mod.rs
@@ -1,2 +1,9 @@
 mod create_text_post;
-mod post_list;
+
+#[ft_sdk::data]
+fn post_list(
+    mut me: backend::MaybeMe,
+    ft_sdk::Json(input): ft_sdk::Json<backend::db::ListInput>,
+) -> ft_sdk::data::Result {
+    ft_sdk::data::json(input.fetch(&mut me.conn, &me.ud)?)
+}
--- backend/src/handlers/post_list.rs
+++
@@ -1,7 +0,0 @@
-#[ft_sdk::data]
-fn post_list(
-    mut me: backend::MaybeMe,
-    ft_sdk::Json(input): ft_sdk::Json<backend::db::ListInput>,
-) -> ft_sdk::data::Result {
-    ft_sdk::data::json(input.fetch(&mut me.conn, &me.ud)?)
-}

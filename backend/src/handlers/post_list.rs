ad1962dd484f3a165a85a595291a382611a6a547
Amit Upadhyay <upadhyay@gmail.com>
2025-03-02 22:26:15 +0530

    slightly better(?) check

--- backend/src/handlers/post_list.rs
+++ backend/src/handlers/post_list.rs
@@ -4,19 +4,20 @@ fn post_list(
     app_url: ft_sdk::AppUrl,
     ft_sdk::Default(tags): ft_sdk::Default<"tags", Vec<String>>,
     ft_sdk::Default(since): ft_sdk::Default<"since", Option<String>>,
-    ft_sdk::Default(per_page): ft_sdk::Default<"per-page", Option<u8>>,
+    per_page: ft_sdk::Default<"per-page", Option<u8>>,
 ) -> ft_sdk::data::Result {
-    let per_page = per_page.unwrap_or(10);
-    if per_page > 100 {
-        return Err(ft_sdk::single_error("per-page", "Can not return more than 100").into());
-    }
-
+    let per_page = per_page
+        .check(|p| p > &Some(100), "Can not return more than 100 rows")?
+        .get()
+        .unwrap_or(10);
     let mut posts = backend::db::ListInput {
         tags,
         since,
         per_page,
     }
         .fetch(&mut me.conn, &me.ud, me.is_admin)?;
-    posts.iter_mut().for_each(|u| u.permalink = app_url.join(&u.permalink).unwrap());
+    posts
+        .iter_mut()
+        .for_each(|u| u.permalink = app_url.join(&u.permalink).unwrap());
     ft_sdk::data::json(posts)
 }

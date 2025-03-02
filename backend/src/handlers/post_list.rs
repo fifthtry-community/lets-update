7589080954a69eef42f3d156c92d5712e57530fb
Amit Upadhyay <upadhyay@gmail.com>
2025-03-02 15:37:40 +0530

    a bit more idiomatic Rust

--- backend/src/handlers/post_list.rs
+++ backend/src/handlers/post_list.rs
@@ -3,13 +3,8 @@ fn post_list(
     mut me: backend::MaybeMe,
     app_url: ft_sdk::AppUrl,
 ) -> ft_sdk::data::Result {
-    let posts: Vec<_> = backend::db::ListInput::default()
-        .fetch(&mut me.conn, &me.ud, me.is_admin)?
-        .into_iter()
-        .map(|mut u| {
-            u.permalink = app_url.join(u.permalink).unwrap();
-            u
-        })
-        .collect();
+    let mut posts = backend::db::ListInput::default()
+        .fetch(&mut me.conn, &me.ud, me.is_admin)?;
+    posts.iter_mut().for_each(|u| u.permalink = app_url.join(&u.permalink).unwrap());
     ft_sdk::data::json(posts)
 }

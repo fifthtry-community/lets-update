57ff34f2b350b200f989a794ecffa5e80b091e5c
Amit Upadhyay <upadhyay@gmail.com>
2025-03-03 21:32:12 +0530

    minor refactor: extract since as var

--- backend/src/handlers/post_list.rs
+++ backend/src/handlers/post_list.rs
@@ -18,14 +18,15 @@ fn post_list(
     let tags = tags
         .map(|t| t.split(',').map(|s| s.to_string()).collect())
         .unwrap_or_default();
+    let since = since
+        .and_then(|s| serde_json::from_str(s.as_str()).ok())
+        .unwrap_or(i64::MAX);
     ft_sdk::data::json(
         backend::db::ListInput {
             tags,
-            since: since
-                .map(|s| serde_json::from_str(s.as_str()).unwrap_or(i64::MAX))
-                .unwrap_or(i64::MAX),
+            since,
             per_page,
         }
-        .fetch(&mut me.conn, &me.ud, me.is_admin, &app_url)?,
+            .fetch(&mut me.conn, &me.ud, me.is_admin, &app_url)?,
     )
 }

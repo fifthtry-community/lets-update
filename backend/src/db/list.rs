5f03ddfe131cc7d9e755cc309550adc9e42240b0
Amit Upadhyay <upadhyay@gmail.com>
2025-03-03 12:02:41 +0530

    more pagination implementation: fastn seems buggy

--- backend/src/db/list.rs
+++ backend/src/db/list.rs
@@ -2,8 +2,7 @@
 pub struct ListInput {
     #[expect(unused)]
     pub tags: Vec<String>,
-    #[expect(unused)]
-    pub since: Option<String>,
+    pub since: i64,
     pub per_page: u8,
 }
 
@@ -26,11 +25,13 @@ impl ListInput {
             cdp_update::table
                 .select(backend::db::DbUpdate::as_select())
                 .limit(i64::from(self.per_page + 1))
+                .filter(cdp_update::id.lt(self.since))
                 .order(cdp_update::id.desc())
                 .load(conn)?
         } else {
             cdp_update::table
                 .select(backend::db::DbUpdate::as_select())
+                .filter(cdp_update::id.lt(self.since))
                 .filter(cdp_update::is_public.eq(true))
                 .limit(i64::from(self.per_page + 1))
                 .order(cdp_update::id.desc())
@@ -50,7 +51,11 @@ impl ListInput {
             updates.push(row.into_update()?);
         }
 
-        Ok(Paged { next, previous: None, items: updates })
+        Ok(Paged {
+            next,
+            previous: None,
+            items: updates,
+        })
     }
 }
 
@@ -61,4 +66,18 @@ pub struct Paged<ITEM: serde::Serialize> {
     // this is what you should use as since for the previous page
     pub previous: Option<String>,
     pub items: Vec<ITEM>,
+}
+
+impl<T: serde::Serialize> Paged<T> {
+    pub fn fix_nav_links(&mut self, app_url: &ft_sdk::AppUrl) -> ft_sdk::Result<()> {
+        if let Some(next) = &self.next {
+            self.next = Some(format!("{}?since={next}", app_url.join("/")?));
+        };
+
+        if let Some(previous) = &self.previous {
+            self.previous = Some(format!("{}?since={previous}", app_url.join("/")?));
+        };
+
+        Ok(())
+    }
 }
--- backend/src/handlers/post_list.rs
+++ backend/src/handlers/post_list.rs
@@ -6,17 +6,22 @@ fn post_list(
     ft_sdk::Default(since): ft_sdk::Default<"since", Option<String>>,
     per_page: ft_sdk::Default<"per-page", Option<u8>>,
 ) -> ft_sdk::data::Result {
+    ft_sdk::println!("since: {since:?}, app_url: {app_url:?}, tags: {tags:?}");
     let per_page = per_page
         .check(|p| p < &Some(100), "Can not return more than 100 rows")?
         .get()
         .unwrap_or(10);
     let mut posts = backend::db::ListInput {
         tags,
-        since,
+        since: since
+            .map(|s| serde_json::from_str(s.as_str()).unwrap_or(i64::MAX))
+            .unwrap_or(i64::MAX),
         per_page,
     }
         .fetch(&mut me.conn, &me.ud, me.is_admin)?;
-    posts.items
+    posts.fix_nav_links(&app_url)?;
+    posts
+        .items
         .iter_mut()
         .for_each(|u| u.permalink = app_url.join(&u.permalink).unwrap());
     ft_sdk::data::json(posts)

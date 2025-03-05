f76e23aafac148f86a9535b74b80346312f94a5d
Amit Upadhyay <upadhyay@gmail.com>
2025-03-05 11:01:28 +0530

    removing hack since fastn issue is fixed

--- backend/src/handlers/post_list.rs
+++ backend/src/handlers/post_list.rs
@@ -1,15 +1,11 @@
 #[ft_sdk::data]
 fn post_list(
     mut me: backend::MaybeMe,
-    mut app_url: ft_sdk::AppUrl,
+    app_url: ft_sdk::AppUrl,
     ft_sdk::Query(tags): ft_sdk::Query<"tags", Option<String>>,
     ft_sdk::Query(since): ft_sdk::Query<"since", Option<String>>,
     per_page: ft_sdk::Default<"per-page", Option<u8>>,
 ) -> ft_sdk::data::Result {
-    if app_url.url.as_ref().map(AsRef::as_ref) == Some("/backend/") {
-        app_url.url = Some("/updates/".to_string());
-    };
-
     ft_sdk::println!("since: {since:?}, app_url: {app_url:?}, tags: {tags:?}");
     let per_page = per_page
         .check(|p| p < &Some(100), "Can not return more than 100 rows")?
@@ -27,6 +23,6 @@ fn post_list(
             since,
             per_page,
         }
-            .fetch(&mut me.conn, &me.ud, me.ud.is_some(), &app_url)?,
+        .fetch(&mut me.conn, &me.ud, me.ud.is_some(), &app_url)?,
     )
 }
